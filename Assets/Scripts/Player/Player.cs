b28231334453ed79d387b82fa6b03bd559cb5766
JP5252 <44101227+JP5252@users.noreply.github.com>
2024-03-04 00:39:52 -0700

    add jumping mechanics

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -9,6 +9,8 @@ public class Player : MonoBehaviour
     [Header("Jump")]
     [SerializeField] private float jumpForce;
     [SerializeField] private float jumpTimeMax;
+    [SerializeField] private float jumpTimeMin;
+    [SerializeField] private float jumpAngle;
 
     [Header("Jump")]
     [SerializeField] private LayerMask whatIsGround;
@@ -20,8 +22,6 @@ public class Player : MonoBehaviour
     private Animator anim;
     private float moveInput;
 
-    private bool isJumping;
-    private bool isFalling;
     private float jumpTimeCounter = 0f;
 
     private RaycastHit2D groundHit;
@@ -37,7 +37,11 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        Move();
+        if (IsGrounded())
+        {
+            Move();
+        }
+        
         Jump();
     }
 
@@ -47,8 +51,10 @@ public class Player : MonoBehaviour
     {
         moveInput = UserInput.instance.moveInput.x;
 
-        if (moveInput > 0 || moveInput < 0)
+        if (moveInput > 0 || moveInput < 0 && IsGrounded())
         {
+            // dont know why i have to set this isJumping false, isGrounded should set it to that
+            anim.SetBool("isJumping", false);
             anim.SetBool("isWalking", true);
             TurnCheck();
         }
@@ -57,7 +63,11 @@ public class Player : MonoBehaviour
             anim.SetBool("isWalking", false);
         }
 
-        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+        // check if player is crouched before moving them
+        if (!anim.GetBool("isCrouching") && !anim.GetBool("isJumping"))
+        {
+            rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+        }
     }
 
     private void Jump()
@@ -65,25 +75,33 @@ public class Player : MonoBehaviour
         if (UserInput.instance.controls.Jumping.Jump.WasPressedThisFrame() && IsGrounded())
         {
             jumpTimeCounter = 0f;
+            anim.SetBool("isCrouching", true);
+            rb.velocity = new Vector2(0f, rb.velocity.y);
         }
 
-        if (UserInput.instance.controls.Jumping.Jump.IsPressed()) 
+        if (UserInput.instance.controls.Jumping.Jump.IsPressed() && IsGrounded())
         {
             jumpTimeCounter += Time.deltaTime;
         }
 
-        if (UserInput.instance.controls.Jumping.Jump.WasReleasedThisFrame())
+        if (UserInput.instance.controls.Jumping.Jump.WasReleasedThisFrame() && IsGrounded())
         {
-            //if the jump is less than max charge
-            if (jumpTimeCounter < jumpTimeMax)
-            {
-                rb.velocity = new Vector2(rb.velocity.x, jumpForce * jumpTimeCounter * 2);
-            }
-            //if the jump is charged to max
-            else
-            {
-                rb.velocity = new Vector2(rb.velocity.x, jumpForce * jumpTimeMax * 2);
-            }
+            // Get the player's input direction using the moveInput
+            moveInput = UserInput.instance.moveInput.x;
+
+            // Calculate jump force based on charge time
+            float chargeFactor = Mathf.Clamp01(jumpTimeCounter / jumpTimeMax);
+            float appliedJumpForce = Mathf.Lerp(jumpForce * jumpTimeMin, jumpForce * jumpTimeMax, chargeFactor);
+
+            // Apply constant horizontal velocity during jump
+            float horizontalVelocity = moveInput * jumpAngle;
+
+            // Apply the calculated velocities
+            rb.velocity = new Vector2(horizontalVelocity, appliedJumpForce);
+
+            // Set appropriate animation states
+            anim.SetBool("isCrouching", false);
+            anim.SetBool("isJumping", true);
         }
     }
 
@@ -99,17 +117,46 @@ public class Player : MonoBehaviour
     /// </returns>
     private bool IsGrounded()
     {
+        // Perform boxcast to check for any collision
         groundHit = Physics2D.BoxCast(col.bounds.center, col.bounds.size, 0f, Vector2.down, 0.25f, whatIsGround);
 
+        // Check if the player is touching any collider
         if (groundHit.collider != null)
         {
-            return true;
+            // Check if the normal of the collision is not completely vertical (indicating a wall)
+            if (Vector2.Dot(groundHit.normal, Vector2.up) < 0.95f)
+            {
+                // Bounce off the wall
+                Vector2 wallNormal = groundHit.normal;
+                rb.velocity = Vector2.Reflect(rb.velocity, wallNormal);
+                return false;
+            }
+            else
+            {
+                // If the normal is vertical, treat it as ground
+                anim.SetBool("isJumping", false);
+                anim.SetBool("isFalling", false);
+                return true;
+            }
         }
         else
         {
+            // If no collider is hit, treat it as falling
+            if (rb.velocity.y < 0)
+            {
+                anim.SetBool("isFalling", true);
+            }
+            else
+            {
+                anim.SetBool("isFalling", false);
+            }
             return false;
         }
     }
+
+
+
+
 
     #endregion
 
