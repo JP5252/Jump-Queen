a300df33d7c13b65d2ee7b187f0cdbdb17c1e2ad
benwalsh23 <walshb23@mymacewan.ca>
2024-03-18 18:05:04 -0600

    gem counter

--- Assets/Scripts/gems.cs
+++ Assets/Scripts/gems.cs
@@ -1,28 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class gems : MonoBehaviour
 {
-    public Text gemCounterText; // Reference to the UI Text element for displaying gem count
-    private int gemCount = 0;   // Counter for tracking gem count
+    public Text gemCounterText;
+    public static int gemCount = 0;
 
-    // Called when another collider enters the trigger
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            // Increment gem count
             gemCount++;
             UpdateGemCounter();
 
-            // Destroy the gem GameObject
             Destroy(gameObject);
         }
     }
 
-    // Update the gem counter UI Text
     private void UpdateGemCounter()
     {
         if (gemCounterText != null)
@@ -30,5 +24,4 @@ public class gems : MonoBehaviour
             gemCounterText.text = "Gems: " + gemCount;
         }
     }
-
 }
